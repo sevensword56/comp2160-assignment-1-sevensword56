164e778053208444681085b94fa4bee4e4003de7
Nat Stepan <natstepan8766@gmail.com>
2020-08-30 13:27:26 +1000

    Implemented Walls, Player & Bullets

---
+++ COMP2160 Assignment 1/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float yMovement = 1.0f;
+    public float xMovement = 1.0f;
+    public float bulletSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementVertical = (yMovement * bulletSpeed) * Time.deltaTime;
+        float movementHorizontal = (xMovement * bulletSpeed) * Time.deltaTime;
+
+        transform.Translate(movementHorizontal, movementVertical, 0);
+    }
+}
---
+++ COMP2160 Assignment 1/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 1.0f;
+    public Bullet prefab;
+    public float bulletDelay = 1.0f;
+
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Timer update
+        timer -= Time.deltaTime;
+
+        // Player Movement
+        float movementVertical = (Input.GetAxis("Vertical") * playerSpeed) * Time.deltaTime;
+        float movementHorizontal = (Input.GetAxis("Horizontal") * playerSpeed) * Time.deltaTime;
+
+        transform.Translate(movementHorizontal, movementVertical, 0);
+
+        // Player Shooting
+        int fireVertical = 0;
+        int fireHorizontal = 0;
+
+        if(Input.GetAxis("Vertical Fire") > 0)
+            fireVertical = 1;
+        else if(Input.GetAxis("Vertical Fire") < 0)
+            fireVertical = -1;
+        
+        if(Input.GetAxis("Horizontal Fire") > 0)
+            fireHorizontal = 1;
+        else if(Input.GetAxis("Horizontal Fire") < 0)
+            fireHorizontal = -1;
+
+        if((fireVertical != 0 || fireHorizontal != 0) && timer <= 0)
+        {
+            timer = bulletDelay;
+            Bullet bullet = Instantiate(prefab);
+            bullet.transform.position = transform.position;
+            bullet.yMovement = fireVertical;
+            bullet.xMovement = fireHorizontal;
+        }
+    }
+}
