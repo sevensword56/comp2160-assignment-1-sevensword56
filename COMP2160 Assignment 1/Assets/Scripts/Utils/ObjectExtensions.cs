8149298cc13fad7a892dae824054c31aec9ba82d
Malcolm Ryan <malcolm.ryan@mq.edu.au>
2020-08-30 11:15:44 +1000

    Initial commit

---
+++ COMP2160 Assignment 1/Assets/Scripts/Utils/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Extension methods for the Object class
+ */
+
+public static class ObjectExtensions  {
+
+	/**
+	 * A simplified version of Object.FindObjectOfType() that eliminates the need for type-casting.
+	 */
+
+	public static T FindObjectOfType<T>(this Object o) where T : Object {
+		return (T) Object.FindObjectOfType (typeof(T));
+	}
+		
+	/**
+	 * A simplified version of Object.FindObjectsOfType() that eliminates the need for type-casting.
+	 */
+
+	public static T[] FindObjectsOfType<T>(this Object o) where T : Object {
+		return (T[]) Object.FindObjectsOfType (typeof(T));
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+COMP2160 Game Development 
+Assignment 1
+
+Please edit this file to include your name and student number.
+
+Full name: 
+Student ID:
+
